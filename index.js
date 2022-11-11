309cf83ffaf10ff1a09177b749fcf08fdf31d344
ChurroC <charanvenkatchandran@gmail.com>
2022-11-10 22:52:14 -0600

    Thought it would work

--- index.js
+++ index.js
@@ -2,65 +2,100 @@
 
 const fs = require('fs')
 const path = require('path')
-const readline = require('linebyline')
+const { config } = require('process')
 
-async function folderWithGit(targetPath, findDir) {
-    const files = await fs.promises.readdir(targetPath)
-    const result = []
-
-    for (let file of files) {
-        try {
-            const filepath = path.join(targetPath, file)
-            const stats = await fs.promises.lstat(filepath)
-
-            if (stats.isDirectory()) {
-                const childFiles = await fs.promises.readdir(filepath)
-                if (childFiles.includes(findDir)) {
-                    result.push(filepath)
+function traverseDir(dir, ignore) {
+    const dirs = []
+    fs.readdirSync(dir).forEach(file => {
+        let fullPath = path.join(dir, file)
+        if (!ignore.includes(fullPath) || !fullPath.split('/')[fullPath.split('/').length - 1] == 'node_modules') {
+            if (fs.lstatSync(fullPath).isDirectory()) {
+                if (fs.readdirSync(fullPath).includes('.git')) {
+                    dirs.push(fullPath)
+                } else {
+                    dirs.push(...traverseDir(fullPath, ignore))
                 }
-                files.push(...childFiles.map(f => path.join(file, f)))
             }
-        } catch (err) {
-            console.error(err)
         }
-    }
-    return result
+    })
+    return dirs
+}
+//So if a folder has a git it'll still keep going and tryign to check for deeper .git folders
+function traverseDirGitInGit(dir, ignore) {
+    const dirs = []
+    fs.readdirSync(dir).forEach(file => {
+        let fullPath = path.join(dir, file)
+        if (!ignore.includes(fullPath) || !fullPath.split('/')[fullPath.split('/').length - 1] == 'node_modules') {
+            if (fs.lstatSync(fullPath).isDirectory()) {
+                if (fs.readdirSync(fullPath).includes('.git')) {
+                    dirs.push(fullPath)
+                }
+                dirs.push(...traverseDir(fullPath, ignore))
+            }
+        }
+    })
+    return dirs
 }
 
-function addSubmodule(targetDir) {
-    const result = folderWithGit('C:/Brothers/Charan/Code/Javascript/Projects', '.git')
-    for (let dir of result) {
-        const data = fs.readFileSync(path.join(dir, '.git', 'config'))
-        console.log(data)
-    }
-    const message = `[submodule "React-Calculator"]/n\tpath = Javascript/Projects/calculator_react_app\n\turl = https://github.com/ChurroC/React-Calculator`
-    fs.writeFileSync(path.join(targetDir, '.gitmodules'), message)
+function addAllSubmodules(dirs) {
+    let gitModuleMessage = ''
+    let configMessage = '\n'
+    dirs.forEach(dirWithoutGit => {
+        try {
+            const dir = path.join(dirWithoutGit, '.git')
+            let lineTrue = false
+            let url = ''
+            const lines = fs.readFileSync(path.join(dir, 'config'), 'utf-8').split('\n')
+            lines.forEach(line => {
+                if (lineTrue) {
+                    url = line.split(' ')[2]
+                    lineTrue = false
+                }
+                if (line == '[remote "origin"]') {
+                    lineTrue = true
+                    console.log('found origin')
+                }
+            })
+            const urlSplit = url.split('/').pop().split('.')[0]
+            console.log(urlSplit)
+            dirWithoutGit = dirWithoutGit.split('\\').splice(4).join('\\')
+            gitModuleMessage += `[submodule "${urlSplit}"]\n\tpath = ${dirWithoutGit}\n\turl = ${url}\n`
+            configMessage += `[submodule "${urlSplit}"]\n\turl = ${url}\n\tactive = true\n`
+        } catch (error) {
+            console.log(error)
+        }
+    })
+    return [gitModuleMessage, configMessage]
 }
 
-const run = async () => {
-    try {
-        const found = await search('C:/Brothers/Charan/Code/Javascript/Projects', '.git')
-        console.log(found)
-    } catch (err) {
-        console.error(err)
-    }
-}
+const startDir = path.join('C:', 'Brothers', 'Charan', 'Code')
+const allGit = traverseDir(startDir, [
+    path.join(startDir, '.git'),
+    path.join(startDir, 'SandBox'),
+    path.join(startDir, 'Tutorials'),
+])
+console.log(allGit)
+const [gitModuleMessage, configMessage] = addAllSubmodules(allGit)
+fs.writeFileSync(path.join(startDir, '.gitmodules'), gitModuleMessage)
+fs.appendFileSync(path.join(startDir, '.git', 'config'), configMessage)
 
-//addSubmodule('C:/Brothers/Charan/Code')
+/*
 let lineTrue = false
+let url = ''
 let lines = fs
     .readFileSync('C:/Brothers/Charan/Code/Javascript/Projects/calculator_react_app/.git/config', 'utf-8')
     .split('\n')
 for (let line of lines) {
     if (lineTrue) {
-        const url = line.split(' ')[2]
-        console.log(url)
+        url = line.split(' ')[2]
         break
     }
     if (line == '[remote "origin"]') {
         lineTrue = true
     }
 }
+let message = `[submodule "${url.split('/').pop()}"]\n\tpath = Javascript/Projects/calculator_react_app\n\turl = ${url}`
+fs.writeFileSync('./.gitmodules', message)*/
 /*
 let lineTrue = false
 rd.on('line', function (line, lineCount, byteCount) {
